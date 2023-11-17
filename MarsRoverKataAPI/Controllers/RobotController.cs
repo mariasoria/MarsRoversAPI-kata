7a2d2e93312810650d91ea360a04692079d8401d
Maria Soria <maria.it3@gmail.com>
2023-11-17 14:32:44 +0000

    refactor: add and integrate RobotResponse class

--- MarsRoverKataAPI/Controllers/RobotController.cs
+++ MarsRoverKataAPI/Controllers/RobotController.cs
@@ -22,13 +22,30 @@ public class RobotController : ControllerBase
         var initialPosition = new Position(request.X, request.Y);
         // ToDo. Validation of the input being part of an enum?
         // ToDo. Parse from string to enum?? Replace that Direction.North
-        var robot = Robot.Create(initialPosition, Direction.North);
+        var robot = Robot.Create(initialPosition, Services.Directions.Direction.North);
         var remoteControl = new RemoteControl(robot);
         var commands = request.Commands;
-        
+
         remoteControl.Execute(commands);
-        
+
+        var state = robot.State;
+        var direction = Direction(state);
+        // var position = Position(state);
         logger.LogInformation("Todo ok, por ahora");
-        return Ok("Todo ok, por ahora");
+        return Ok(new RobotResponse(direction, 0, 0));
+    }
+
+    public static string Direction(State state)
+    {
+        var direction = "";
+        if (state.GetType() == typeof(North))
+            direction = "North";
+        else if (state.GetType() == typeof(South))
+            direction = "South";
+        else if (state.GetType() == typeof(East))
+            direction = "East";
+        else if (state.GetType() == typeof(West))
+            direction = "West";
+        return direction;
     }
 }
